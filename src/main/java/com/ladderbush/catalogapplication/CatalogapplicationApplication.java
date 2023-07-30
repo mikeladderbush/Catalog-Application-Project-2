c52d52dcb785834f7bc23b307179a714c963155a
Michael Ladderbush <38595703+mikeladderbush@users.noreply.github.com>
2023-07-30 14:40:07 -0400

    First commit for project creation

---
+++ src/main/java/com/ladderbush/catalogapplication/CatalogapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ladderbush.catalogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ladderbush/catalogapplication/CatalogapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladderbush.catalogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
